4bacc0706a3ff6a2d7ec62bd15e52454c45fe84b
Breno Santiago De Sousa <dev.santiago@proton.me>
2025-05-12 21:12:00 -0300

    refactor: Enhance Add Transaction Screen UI and Navigation
    This commit refactors the Add Transaction screen to improve its user interface and adds a button for navigating back to the dashboard.
    
    Changes include:
    - Added a "Add Transaction" title to the screen using `MaterialTheme.typography.headlineMedium`.
    - Added a `Box` with padding to wrap the buttons.
    - Added a `Row` within the `Box` to arrange the "Add Transaction" and "Back To Dashboard" buttons horizontally.
    - Added a "Back To Dashboard" button that navigates back to the "home" route using `navController.navigate("home")`.
    - Added a `Spacer` to add space between the buttons.
    - Initialized default project inspection profiles in `.idea/inspectionProfiles/Project_Default.xml`.

--- app/src/main/java/com/devsant/fintrack/ui/screens/AddTransaction.kt
+++ app/src/main/java/com/devsant/fintrack/ui/screens/AddTransaction.kt
@@ -1,10 +1,16 @@
 package com.devsant.fintrack.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,12 +20,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.devsant.fintrack.viewmodel.TransactionViewModel
 
 @Composable
 fun AddTransaction(modifier : Modifier = Modifier, navController: NavHostController) {
@@ -35,10 +39,34 @@ fun AddTransaction(modifier : Modifier = Modifier, navController: NavHostControl
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(text = "Add Transaction", style = MaterialTheme.typography.headlineMedium)
         OutlinedTextField(value = date, onValueChange = { date = it }, label = { Text("Date") }, modifier = Modifier.fillMaxWidth())
         OutlinedTextField(value = amount, onValueChange = { amount = it }, label = { Text("Amount") }, modifier = Modifier.fillMaxWidth())
         OutlinedTextField(value = category, onValueChange = { category = it }, label = { Text("Category") }, modifier = Modifier.fillMaxWidth())
         OutlinedTextField(value = type, onValueChange = { type = it }, label = { Text("Type") }, modifier = Modifier.fillMaxWidth())
+
+        Box(
+            modifier = Modifier
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(onClick = { }) {
+                    Text("Add Transaction")
+                }
+                Spacer(modifier = Modifier.height(100.dp))
+                Button(onClick = { navController.navigate("home") }) {
+                    Text("Back To DashBoard")
+                }
+            }
+
+        }
     }
 }
 
