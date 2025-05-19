7e65b9fc53c7cdafdaeb425fb9cb36229f88d588
Breno Santiago De Sousa <dev.santiago@proton.me>
2025-05-19 19:23:07 -0300

    feat: Implement Transaction Detail Screen and Navigation
    This commit introduces the Transaction Detail Screen, allowing users to view the details of a specific transaction. It also updates navigation to enable access to this screen from the Home Screen.
    
    Changes include:
    - Created `TransactionDetailScreen.kt` to display transaction details (title, date, amount, category, type) and provide "Edit" and "Delete" buttons (functionality not yet implemented).
    - Modified `AppNavigation.kt` to include a route for `TransactionDetailScreen`, accepting a `transactionId` as an argument.
    - Updated `HomeScreen.kt`:
        - `HomeScreenContent` now accepts a `NavHostController`.
        - Removed the `onTransactionClick` lambda from `HomeScreenContent` as navigation is handled directly in `TransactionCard`.
    - Updated `TransactionCard.kt`:
        - Now accepts a `NavHostController`.
        - Clicking a `TransactionCard` navigates to `transactionDetailScreen` with the corresponding `transaction.id`.

--- app/src/main/java/com/devsant/fintrack/ui/screens/TransactionDetailScreen.kt
+++ app/src/main/java/com/devsant/fintrack/ui/screens/TransactionDetailScreen.kt
@@ -5,10 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -17,9 +21,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.devsant.fintrack.model.Transaction
 import com.devsant.fintrack.viewmodel.TransactionViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,19 +38,28 @@ fun TransactionDetailScreen(
 ) {
     val transaction = viewModel.getTransactionById(transactionId)
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text("FinTrack", color = Color.White, fontWeight = FontWeight.Bold)
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color(0xFF1B213F)
+
+    transaction?.let {
+        viewModel.title.value = it.title
+        viewModel.date.value = it.date
+        viewModel.amount.value = it.amount
+        viewModel.category.value = it.category
+        viewModel.type.value = it.type
+
+
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text("FinTrack", color = Color.White, fontWeight = FontWeight.Bold)
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color(0xFF1B213F)
+                    )
                 )
-            )
-        }
-    ) { innerPadding ->
-        transaction?.let {
+            }
+        ) { innerPadding ->
             Column(
                 modifier = Modifier
                     .padding(innerPadding)
@@ -51,17 +67,82 @@ fun TransactionDetailScreen(
                     .fillMaxSize(),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                Text("Title: ${it.title}", fontWeight = FontWeight.Bold)
-                Text("Date: ${it.date}")
-                Text("Amount: ${it.amount}")
-                Text("Category: ${it.category}")
-                Text("Type: ${it.type}")
+                OutlinedTextField(
+                    value = viewModel.title.value,
+                    onValueChange = { viewModel.title.value = it },
+                    label = { Text("Title") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.medium,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color(0xFF1B213F),
+                        unfocusedBorderColor = MaterialTheme.colorScheme.surface,
+                    )
+                )
+                OutlinedTextField(
+                    value = viewModel.date.value,
+                    onValueChange = { viewModel.date.value = it },
+                    label = { Text("Date") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.medium,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color(0xFF1B213F),
+                        unfocusedBorderColor = MaterialTheme.colorScheme.surface,
+                    )
+                )
+                OutlinedTextField(
+                    value = viewModel.amount.value,
+                    onValueChange = { viewModel.amount.value = it },
+                    label = { Text("Amount") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.medium,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color(0xFF1B213F),
+                        unfocusedBorderColor = MaterialTheme.colorScheme.surface,
+                    )
+                )
+                OutlinedTextField(
+                    value = viewModel.category.value,
+                    onValueChange = { viewModel.category.value = it },
+                    label = { Text("Category") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.medium,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color(0xFF1B213F),
+                        unfocusedBorderColor = MaterialTheme.colorScheme.surface,
+                    )
+                )
+                OutlinedTextField(
+                    value = viewModel.type.value,
+                    onValueChange = { viewModel.type.value = it },
+                    label = { Text("Type") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.medium,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color(0xFF1B213F),
+                        unfocusedBorderColor = MaterialTheme.colorScheme.surface,
+                    )
+                )
 
                 Spacer(modifier = Modifier.height(24.dp))
 
                 Row(horizontalArrangement = Arrangement.spacedBy(16.dp)){
-                    Button(onClick = { /*EDIT LOGIC*/ }) {
-                        Text("Edit")
+                    Button(onClick = {
+                        viewModel.updateTransaction(
+                            id = transaction.id,
+                            title = viewModel.title.value,
+                            date = viewModel.date.value,
+                            amount = viewModel.amount.value,
+                            category = viewModel.category.value,
+                            type = viewModel.type.value
+                        )
+
+                    }) {
+                        Text("Save")
                     }
                     Button(onClick = { /*DELETE LOGIC*/ }) {
                         Text("Delete")
@@ -69,6 +150,37 @@ fun TransactionDetailScreen(
                 }
             }
         }
-
     }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TransactionDetailScreenPreview() {
+    // Fake transaction for preview
+    val fakeTransaction = Transaction(
+        id = 1,
+        title = "Groceries",
+        amount = "150.0",
+        date = "19/05/2025",
+        category = "Food",
+        type = "Expense"
+    )
+
+    // Fake ViewModel
+    val fakeViewModel = object : TransactionViewModel() {
+        override fun getTransactionById(id: Int): Transaction? {
+            return fakeTransaction
+        }
+    }
+
+    // Fake NavController
+    val fakeNavController = rememberNavController()
+
+    TransactionDetailScreen(
+        transactionId = 1,
+        navController = fakeNavController,
+        viewModel = fakeViewModel
+    )
 }
