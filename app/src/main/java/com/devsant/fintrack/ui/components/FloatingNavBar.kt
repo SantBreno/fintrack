b4bc853992e7a93860cbebab339d199b69e43758
Breno Santiago De Sousa <dev.santiago@proton.me>
2025-07-23 17:00:03 -0300

    feat: Create placeholder for FloatingNavBar composable
    This commit introduces an empty `FloatingNavBar.kt` file within the `com.devsant.fintrack.ui.components` package.
    
    This composable function is intended to implement a floating navigation bar and currently accepts a `NavHostController` and an optional `Modifier`. Further implementation will be added in subsequent commits.

---
+++ app/src/main/java/com/devsant/fintrack/ui/components/FloatingNavBar.kt
@@ -0,0 +1,12 @@
+package com.devsant.fintrack.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun FloatingNavBar(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+}
